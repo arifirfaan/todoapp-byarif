2937cdde0d29fcc5fc9d9b54e1d9ffc2985d8ebd
arif_laptop <arifirfaanworks@gmail.com>
2022-03-03 23:57:08 +0800

    start create home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tutorialf/widget/dynamic_listview.dart';
+import 'package:tutorialf/widget/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -44,25 +44,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (BuildContext context){return DynamicListView();})
-                );
-              },
-              child: Text('Dynamic Listview'),
-            )
-          ],
-        ),
-      ),
-    );
+    return Home();
   }
 }
